df156d9e2a87eaad072af15e09cd0b9240c9f0ef
DUNNA SNEHA <dunnasneha5@gmail.com>
2026-02-01 18:52:21 +0530

    Add files via upload

---
+++ pattern1.py
@@ -0,0 +1,11 @@
+#prattern 1
+#* * * * 
+#* * * * 
+#* * * * 
+#* * * * 
+n=int(input( ))
+for i in range(n):
+    for j in range (n):
+        print("*",end=" ")
+    print()
+print("---")
---
+++ pattern2.py
@@ -0,0 +1,13 @@
+#         * 
+#       * * 
+#     * * * 
+#   * * * * 
+# * * * * * 
+n=int(input("no of rows :"))
+for i in range(n):
+    for j in range (n-i-1):
+        print(" ",end=" ")
+    for j in range(i+1):
+        print("*",end=" ")
+    print()
+    
